9b660c6495c1770e78864744e5107a029c99817e
matcinks <x.mateuszstepien@gmail.com>
2024-05-31 21:04:52 +0200

    Implemented solution for exercise: Calculator Conundrum

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,47 @@
+
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        int result = -1;
+        String[] operations = {"+", "*", "/"};
+
+        if (operation == null) throw new IllegalArgumentException("Operation cannot be null");
+        if (operation.isEmpty()) throw new IllegalArgumentException("Operation cannot be empty");
+        if (!isValidOperation(operation, operations)) throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+
+        result = switch (operation) {
+            case "+" -> addition(operand1, operand2);
+            case "*" -> multiplication(operand1, operand2);
+            case "/" -> division(operand1, operand2);
+            default -> result;
+        };
+
+        return operand1 + " " + operation + " " + operand2 + " = " + result;
+    }
+
+    private static boolean isValidOperation(String operation, String[] operations) {
+        boolean result = false;
+        for (String o : operations) {
+            if (o.equals(operation)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static int addition (int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+
+    private static int multiplication (int operand1, int operand2) {
+        return operand1 * operand2;
+    }
+
+    private static int division (int operand1, int operand2) {
+        try {
+            return operand1 / operand2;
+        } catch (ArithmeticException e) {
+            throw new IllegalOperationException("Division by zero is not allowed", e);
+        }
     }
 }
