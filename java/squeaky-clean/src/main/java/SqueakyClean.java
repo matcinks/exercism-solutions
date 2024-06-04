f2f64acc85039973c75ca7dda320fc6c719853fd
matcinks <x.mateuszstepien@gmail.com>
2024-06-04 11:25:31 +0200

    Refactored alternate solution for exercise: Squeaky Clean

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -3,27 +3,32 @@ class SqueakyClean {
     static String clean(String identifier) {
         StringBuilder result = new StringBuilder(identifier.length());
         for (int i = 0; i < identifier.length(); i++) {
-            char nextCar = identifier.charAt(i);
-            if (Character.isWhitespace(nextCar)) {
+            char nextCharacter = identifier.charAt(i);
+            if (Character.isWhitespace(nextCharacter)) {
                 result.append('_');
-            } else if (nextCar == '-') {
-                result.append(Character.toUpperCase(identifier.charAt(i + 1)));
-                i++;
-            } else if (Character.isDigit(nextCar)) {
-                result.append(
-                        switch (nextCar) {
-                            case '0' -> 'o';
-                            case '1' -> 'l';
-                            case '3' -> 'e';
-                            case '4' -> 'a';
-                            case '7' -> 't';
-                            default -> nextCar;
-                        }
-                );
-            } else if (Character.isLetter(nextCar)) {
-                result.append(nextCar);
+            } else if (nextCharacter == '-') {
+                result.append(convertToCamelCase(identifier.charAt(++i)));
+            } else if (Character.isDigit(nextCharacter)) {
+                result.append(convertLeetSpeakToNormalText(nextCharacter));
+            } else if (Character.isLetter(nextCharacter)) {
+                result.append(nextCharacter);
             }
         }
         return result.toString();
+    }
+
+    private static char convertLeetSpeakToNormalText(char nextCharacter) {
+        return switch (nextCharacter) {
+            case '0' -> 'o';
+            case '1' -> 'l';
+            case '3' -> 'e';
+            case '4' -> 'a';
+            case '7' -> 't';
+            default -> nextCharacter;
+        };
+    }
+
+    private static char convertToCamelCase(char nextCharacter) {
+        return Character.toUpperCase(nextCharacter);
     }
 }
