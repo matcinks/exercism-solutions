738db6a0338d7dbfe403f35616189cadf990c0f5
matcinks <x.mateuszstepien@gmail.com>
2024-06-04 16:28:42 +0200

    Implemented solution for exercise: Elon's Toy Car

--- java/elons-toy-car/src/main/java/ElonsToyCar.java
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -1,17 +1,27 @@
 public class ElonsToyCar {
+
+    private int distance = 0;
+    private int battery = 100;
+
     public static ElonsToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) ElonsToyCar.buy()  method");
+        return new ElonsToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the ElonsToyCar.distanceDisplay()  method");
+        return "Driven " + distance + " meters";
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the ElonsToyCar.batteryDisplay()  method");
+        if (battery <= 0) {
+            return "Battery empty";
+        }
+        return "Battery at " + battery + "%";
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the ElonsToyCar.drive()  method");
+        if (battery > 0) {
+            distance += 20;
+            battery -= 1;
+        }
     }
 }
