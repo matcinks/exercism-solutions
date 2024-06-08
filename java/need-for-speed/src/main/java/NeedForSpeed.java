ab72cec11a465194e1b654f5d406c18f60ac9307
matcinks <x.mateuszstepien@gmail.com>
2024-06-08 13:27:27 +0200

    Implemented solution for exercise: Need for Speed

--- java/need-for-speed/src/main/java/NeedForSpeed.java
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,31 +1,48 @@
 class NeedForSpeed {
+    private final int speed;
+    private final int batteryDrain;
+    private int distance;
+    private int battery = 100;
+
     NeedForSpeed(int speed, int batteryDrain) {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed constructor");
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
     }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return this.battery <= 0;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return this.distance;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (!batteryDrained()) {
+            this.distance += this.speed;
+            this.battery -= this.batteryDrain;
+        }
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
     }
 }
 
 class RaceTrack {
+    private final int distance;
+
     RaceTrack(int distance) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack constructor");
+        this.distance = distance;
     }
 
     public boolean tryFinishTrack(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
+        while (!car.batteryDrained()) {
+            car.drive();
+            if (car.distanceDriven() >= this.distance) {
+                return true;
+            }
+        }
+        return false;
     }
 }
