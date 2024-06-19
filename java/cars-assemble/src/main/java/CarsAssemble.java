1724c9174585036e03f31c952af312abb3565c29
matcinks <x.mateuszstepien@gmail.com>
2024-06-19 10:56:12 +0200

    Implemented solution for exercise: Cars, Assemble

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,24 @@
 public class CarsAssemble {
 
+    private static final int CARS_PER_HOUR = 221;
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        return CARS_PER_HOUR * speed * successRateMultiplier(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) productionRatePerHour(speed) / 60;
+    }
+
+    private double successRateMultiplier(int speed) {
+        if (speed > 9) {
+            return 0.77;
+        } else if (speed > 8) {
+            return 0.8;
+        } else if (speed > 4) {
+            return 0.9;
+        } else {
+            return 1.0;
+        }
     }
 }
